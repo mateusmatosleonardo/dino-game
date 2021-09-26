83ef79f38e89af41dc7a597b67911308c5a8f040
Mateus Leonardo <m4teusmatos@gmail.com>
2021-09-26 18:12:12 -0300

    script: creating variable of isJump

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 const dino = document.querySelector('.dino')
+let isJump = false
 
 function handleKeyUp(event){
     if(event.keyCode === 32){
-        jump()
+        if(!isJump){
+            jump()
+        }
     }
 }
 
 function jump(){
     let position = 0; // posição inicial
+    isJump = true
     let upInterval = setInterval(()=>{
         if(position >= 150){
             clearInterval(upInterval)
-
+            isJump = false
         let downInterval = setInterval(()=>{
             if(position <= 0){
                 clearInterval(downInterval)
