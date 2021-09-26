c8d9f8265857967e19ecc7fdb768542e0320afac
Mateus Leonardo <m4teusmatos@gmail.com>
2021-09-26 18:42:14 -0300

    finalizing scripts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const dino = document.querySelector('.dino')
 const bg = document.querySelector('.bg')
+let position = 0;
 let isJump = false
 
 function handleKeyUp(event){
@@ -11,7 +12,7 @@ function handleKeyUp(event){
 }
 
 function jump(){
-    let position = 0; // posição inicial
+    // posição inicial
     isJump = true
     let upInterval = setInterval(()=>{
         if(position >= 150){
@@ -36,13 +37,25 @@ function jump(){
 function createCactus(){
     const cactus = document.createElement('div')
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000
     cactus.classList.add('cactus')
     cactus.style.left = 1000 + 'px'
     bg.appendChild(cactus)
     let leftInterval = setInterval(()=>{
-        cactusPosition -= 10;
+        cactusPosition -= 6;
         cactus.style.left = cactusPosition + 'px'
+        if(cactusPosition < 60){
+            clearInterval(leftInterval)
+            bg.removeChild(cactus)
+        }else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(leftInterval)
+            document.body.innerHTML = '<h1 class="game-hover">Fim de jogo</h1>';
+        }else{
+            cactusPosition -= 6;
+            cactus.style.left = cactusPosition + 'px'
+        }
     }, 20)
+    setTimeout(createCactus, randomTime)
 }
 createCactus()
 document.addEventListener('keyup', handleKeyUp)
